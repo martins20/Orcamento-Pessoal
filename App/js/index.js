72f5a7ad879db44bc500ee321e4b7c551082e80f
martins20 <55673235+martins20@users.noreply.github.com>
2019-11-06 21:42:13 -0300

    Att
    Melhorias no Script e adicionais (Geração de botões via JS)

--- App/js/index.js
+++ App/js/index.js
@@ -1,22 +1,59 @@
 function btnStart () { 
 
-    let titulo = document.getElementById('modal_titulo') 
-    let classeTitulo = document.getElementById('modal_tituloDiv')
-    let descricao = document.getElementById('modal_desc')
-    let btnModal = document.getElementById('button')
-    let btnConsulta = document.getElementById('buttonConsulta')
+    editarModal ()
+    $('#footer').on('click', '#buttonInserir', function(){
+        window.location.href = 'inserirDespesa.html'
+    });
 
-    titulo.innerHTML = 'Oq deseja fazer ?'
-    classeTitulo.className  = 'modal-header text-primary'
-    descricao.innerHTML = 'Selecione a operação que deseja fazer'
-    btnModal.className = 'btn btn-outline-primary'
-    btnModal.innerHTML = 'inserir despesas'
-    btnConsulta.innerHTML = 'Consultar despesas'
-    btnConsulta.className = 'btn btn-outline-warning'
+    $('#footer').on('click', '#buttonDespesa', function(){
+        window.location.href = 'consulta.html'
+    });
+
 
     $('#modalInicial').modal('show')
 }
 
-function despesa () {
-    window.location.href = './../App/index.html'
+function test() {
+     
+    let btn1 = document.getElementById('buttonInserir')
+    let btn2 = document.getElementById('buttonDespesa')
+
+    btn1.remove()
+    btn2.remove()
+    
+}
+
+function editarModal () {
+
+    let titulo = document.getElementById('modal_titulo') 
+    let classeTitulo = document.getElementById('modal_tituloDiv')
+    let descricao = document.getElementById('modal_desc')
+    let footer = document.getElementById('footer')
+
+    titulo.innerHTML = 'Oq deseja fazer ?'
+    classeTitulo.className  = 'modal-header text-primary'
+    descricao.innerHTML = 'Selecione a operação que deseja fazer'
+
+    //Criação do botão Inserir consulta
+    let button = document.createElement('button')
+    button.setAttribute('id', 'buttonInserir')
+    
+    button.className = 'btn btn-outline-primary'
+    button.innerHTML = 'Inserir Despesas'
+        
+    footer.appendChild(button)
+    
+    //Criação do botão consulta
+    let button2 = document.createElement('button')
+    button2.setAttribute('id', 'buttonDespesa')
+
+    button2.className = 'btn btn-outline-warning'
+    button2.innerHTML = 'Ver consultas'
+
+    footer.appendChild(button2)
+
+}
+
+function clearModal () {
+
 }
